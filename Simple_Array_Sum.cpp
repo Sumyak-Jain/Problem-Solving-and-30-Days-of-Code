6d4d50936606cad130ed1768e15ff881814bffe6
SUMYAK JAIN <3456gaurav@gmail.com>
2020-01-07 13:45:41 +0530

    Simple Array Sum

---
+++ Simple_Array_Sum.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int size,i,sum=0,arr[size],j;
+    cin>>size;
+    for(i=0;i<=size;i++)
+    {
+arr[i]=j;
+cin>>j;
+    }
+    for(i=0;i<=size;i++)
+    {
+        sum=sum+arr[i];
+    }
+    cout<<sum;
+    return 0;
+}
