8ee110d3621a06a351243abea5d327e7b8b5f4c9
SUMYAK JAIN <3456gaurav@gmail.com>
2020-02-12 12:31:20 +0530

    Update Camel_Case.java

--- Camel_Case.java
+++ Camel_Case.java
@@ -3,7 +3,7 @@ public class CamelCase{
     public static void main(String args[]){
         int count=1;//initialize count to 1
         Scanner sc=new Scanner(System.in);
-        String line=sc.nextLine();
+        String line=sc.nextLine();//take input of the string and save it to line
         char[] ch=new char[line.length()];
         for(int i=0;i<line.length();i++){
             ch[i]=line.charAt(i);
