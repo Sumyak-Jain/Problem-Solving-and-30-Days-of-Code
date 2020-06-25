950324f06ad7309fe47bb8f096dd805f4f7421fb
SUMYAK JAIN <3456gaurav@gmail.com>
2020-06-26 03:19:50 +0530

    30 Days Of Code (Day 21)

---
+++ Generics.java
@@ -0,0 +1,38 @@
+import java.util.*;
+
+class Printer <T> {
+  public static < E > void printArray( E[] inputArray ) {
+      // Display array elements
+      for(E element : inputArray) {
+         System.out.printf("%s ", element);
+         System.out.println();
+      }
+      
+   }
+}
+
+public class Generics {
+    
+    public static void main(String args[]){
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        Integer[] intArray = new Integer[n];
+        for (int i = 0; i < n; i++) {
+            intArray[i] = scanner.nextInt();
+        }
+
+        n = scanner.nextInt();
+        String[] stringArray = new String[n];
+        for (int i = 0; i < n; i++) {
+            stringArray[i] = scanner.next();
+        }
+        
+        Printer<Integer> intPrinter = new Printer<Integer>();
+        Printer<String> stringPrinter = new Printer<String>();
+        intPrinter.printArray( intArray  );
+        stringPrinter.printArray( stringArray );
+        if(Printer.class.getDeclaredMethods().length > 1){
+            System.out.println("The Printer class should only have 1 method named printArray.");
+        }
+    } 
+}
